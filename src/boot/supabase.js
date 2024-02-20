c915e1348f31d9be0af6788882daa905aaa70ec0
Carlos Gabriel <carlosgabrx@gmail.com>
2024-02-20 20:39:11 -0300

    add env quasar.conf

--- src/boot/supabase.js
+++ src/boot/supabase.js
@@ -1,8 +1,8 @@
 import { createClient } from '@supabase/supabase-js'
 import useAuthUser from 'src/composables/UseAuthUser'
 
-const supabaseUrl = 'https://ptnziszqasyjheztdwfb.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB0bnppc3pxYXN5amhlenRkd2ZiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDU0MjA3MTcsImV4cCI6MjAyMDk5NjcxN30.OqjoOPygDaRMdvvDSo0x1XHz_lwftoT1izKDQ-K1Bx0'
+const supabaseUrl = process.env.SUPABASE_URL
+const supabaseKey = process.env.SUPABASE_KEY
 const supabase = createClient(supabaseUrl, supabaseKey)
 
 supabase.auth.onAuthStateChange((event, session) => {
